17a9743c70850d3b38baf463110b6dc065a0fefb
ferdigix <ferdinand.leonardi@gmail.com>
2021-06-03 20:13:58 +0700

    Simplify the Flask API & added comments
    Simplify the Flask API, so if the user uploads an image through the Webpage it doesn't need to store the file locally but instead just sends directly to the predict function. Also added comments to explain what each line does.

--- Flask API/flaskapi.py
+++ Flask API/flaskapi.py
@@ -14,52 +14,43 @@ app = flask.Flask(__name__)
 #Load modelnya
 model = tf.keras.models.load_model('cataract_model_basic.h5')
 
-#Directory untuk save image yang di upload via webpagenya
-app.config['UPLOAD_FOLDER'] = "/home/c0050413/capstone-mlmodel/images/"
-
-#Ambil image yang di upload kemudian di save ke folder /images/ dan kemudian di gunakan untuk kirim POST request ke diri sendirinya
-@app.route("/upload-image", methods=["GET", "POST"])
-def upload_file():
-    if flask.request.files:
-        image = flask.request.files["file"]
-        path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
-        image.save(path)
-        images = open(path, "rb").read()
-        payload = {"image": images}
-        r = requests.post("http://35.225.123.148:5000", files=payload).json()
-        return(r)
-    return flask.render_template("webpage.html")
-
 #Tampilin initial webpage
 @app.route('/',methods=['GET'])
 def HelloWorld():
+#Tampilkan webpage
     return flask.render_template("webpage.html")
 
 #Convert Image resolution
 def convert_image(image):
-    if image.mode != "RGB":
-        image = image.convert("RGB")
-    
+    #convert ke RGB (3x8-bit pixels, true color)
+    image = image.convert("RGB")
+    #Resize gambarnya ke resolusi yang model terima (256x256)
     image = image.resize((256,256))
+    #Convert imagenya dari PIL format jadi NumPy array
     image = img_to_array(image)
+    #Expand arraynya 
     image = np.expand_dims(image, axis=0)
-    image = image/255.
-    image = imagenet_utils.preprocess_input(image)
-
+    #Return image
     return image
 
 #Predict
 @app.route('/',methods=['POST'])
 def predict():
+    #Terima Gambar dari POST Request kemudian di baca sebagai bytes
     image = flask.request.files["image"].read()
+    #buka dan identify gambarnya sebagai Binary data
     image = Image.open(io.BytesIO(image))
+    #Convert gambarnya dengan menggunakan function di atas
     image = convert_image(image)
-
+    #Predict gambarnya
     prediction = model.predict(image)
+    #Print hasil prediction di flask api (di gunakan buat saya cek hasil) (kalau tidak mau bisa di comment aja
     print(prediction)
-
+    #Kata team ML model outputnya 0 sampai 1? terus katanya yang 0.5 keatas di jadiin Cataract jadi hasil 0.5 keatas saya return hasilnya sebagai Cataract karena jika tidak nanti outputnya 0 atau 1
     if(float(prediction) >= 0.5):
+        #return output sebagai json
         return json.dumps("Cataract")
+    #Else output Normal
     else:
         return json.dumps("Normal")
 
